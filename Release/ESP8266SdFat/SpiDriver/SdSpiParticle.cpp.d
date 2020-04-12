3043c1d5c5d48464610328273d2c8b02db9fc6d5
GabrielaY <gabrielay2002@gmail.com>
2020-04-12 19:07:21 +0300

    Release folder

---
+++ Release/ESP8266SdFat/SpiDriver/SdSpiParticle.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\gabas\AppData\Local\Temp\VMBuilds\GYTest01\esp8266_nodemcuv2\Release\ESP8266SdFat\SpiDriver\SdSpiParticle.cpp.o: \
+ C:\Users\gabas\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.5.2\libraries\ESP8266SdFat\src\SpiDriver\SdSpiParticle.cpp
